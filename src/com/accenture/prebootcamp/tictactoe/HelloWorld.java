7d4c4da5c9a9d2d90abf2e3b370008f70bdaacd7
AnnaTimofejeva <timofejevaaa@gmail.com>
2020-09-17 21:40:30 +0300

    HelloWorld programm

---
+++ src/com/accenture/prebootcamp/tictactoe/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.accenture.prebootcamp.tictactoe;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello, World!");
+
+        System.out.println("and again");
+    }
+}
