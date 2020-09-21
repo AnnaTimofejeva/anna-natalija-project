46865f619c2219a64d70eb1926c936632119d006
NatalijaBudnik <71456358+NatalijaBudnik@users.noreply.github.com>
2020-09-21 21:00:30 +0300

    done by me2

--- src/com/accenture/prebootcamp/tictactoe/game/Game.java
+++ src/com/accenture/prebootcamp/tictactoe/game/Game.java
@@ -10,12 +10,26 @@ public class Game {
         Moves AllMoves=new Moves();
         GameField newGame=new GameField();
         newGame.printField(AllMoves.FillEmpty());
+        int plNr=1;
+
         do{
-            System.out.println("Your move ");
+            System.out.println("Your move P"+plNr);
             moveAdd=oneMove.nextLine();
             int x=Integer.parseInt(moveAdd.substring(0,1));
             int y=Integer.parseInt(moveAdd.substring(1,2));
-            newGame.printField(AllMoves.NextMove(x,y,"O"));
+            String sign;
+
+            newGame.printField(AllMoves.NextMove(x,y,plNr));
+            if (AllMoves.Winner(plNr)){
+                System.out.println("P"+plNr+" wins!");
+                break;
+            }
+            if (plNr==2){
+                plNr=1;
+            }else{
+                plNr=2;
+            }
+
         }while (moveAdd!="Stop");
 
     }
--- src/com/accenture/prebootcamp/tictactoe/game/Moves.java
+++ src/com/accenture/prebootcamp/tictactoe/game/Moves.java
@@ -2,7 +2,7 @@ package com.accenture.prebootcamp.tictactoe.game;
 
 public class Moves {
 private String[][] moves=new String[3][3];
-
+//Fills array with empty values
     String[][] FillEmpty(){
         for(int i=0; i<3;i++){
             for(int y=0;y<3;y++){
@@ -11,8 +11,78 @@ private String[][] moves=new String[3][3];
         }
         return moves;
     }
-    String[][]NextMove(int x, int y, String sign){
-        moves[x-1][y-1]=sign;
+//Records move into array
+    String[][]NextMove(int x, int y, int plNr){
+        String sign;
+//Based on a player selects a sign
+        if(plNr==1){
+            sign="X";
+        }else{
+            sign="O";
+        }
+//validity check
+        if ((x<=3)&&(y<=3)&&(moves[x-1][y-1]==" ")){
+            moves[x-1][y-1]=sign;
+        }else{
+            System.out.println("Wrong coordinates! Try again!");
+        }
         return moves;
+    }
+
+    Boolean Winner(int plNr){
+        int check=0;
+        String sign;
+        if(plNr==1){
+            sign="X";
+        }else{
+            sign="O";
+        }
+        for (int i=0; i<3; i++){
+            for (int j=0;j<3;j++){
+                if (moves[i][j]==sign){
+                    check++;
+                }
+            }
+            if (check==3){
+                return Boolean.TRUE;
+            }else{
+                check=0;
+            }
+        }
+
+        for (int i=0; i<3; i++){
+            for (int j=0;j<3;j++){
+                if (moves[j][i]==sign){
+                    check++;
+                }
+            }
+            if (check==3){
+                return Boolean.TRUE;
+            }else{
+                check=0;
+            }
+        }
+        for (int i=0; i<3; i++){
+                if (moves[i][i]==sign){
+                    check++;
+                }
+            }
+            if (check==3){
+                return Boolean.TRUE;
+            }else{
+                check=0;
+            }
+
+        for (int i=0; i<3; i++){
+            if (moves[2-i][i]==sign){
+                check++;
+            }
+        }
+        if (check==3){
+            return Boolean.TRUE;
+        }else{
+            check=0;
+        }
+        return Boolean.FALSE;
     }
 }
