55de1b8536b6544e310c13a4ceb4608a257ec0e3
NatalijaBudnik <71456358+NatalijaBudnik@users.noreply.github.com>
2020-09-21 21:26:58 +0300

    done by me2

--- src/com/accenture/prebootcamp/tictactoe/game/Game.java
+++ src/com/accenture/prebootcamp/tictactoe/game/Game.java
@@ -3,33 +3,37 @@ package com.accenture.prebootcamp.tictactoe.game;
 import java.util.Scanner;
 
 public class Game {
+public static int mainPlNr=1;
     public static void main(String[] args) {
         Scanner oneMove=new Scanner(System.in);
         String moveAdd;
 
         Moves AllMoves=new Moves();
         GameField newGame=new GameField();
+//draw empty game field
         newGame.printField(AllMoves.FillEmpty());
-        int plNr=1;
 
+//Start the game
         do{
-            System.out.println("Your move P"+plNr);
+            System.out.println("Your move P"+mainPlNr);
             moveAdd=oneMove.nextLine();
+//determine first coordinate
             int x=Integer.parseInt(moveAdd.substring(0,1));
+//determine second coordinate
             int y=Integer.parseInt(moveAdd.substring(1,2));
-            String sign;
-
-            newGame.printField(AllMoves.NextMove(x,y,plNr));
-            if (AllMoves.Winner(plNr)){
-                System.out.println("P"+plNr+" wins!");
+//Draw game Filed with new move
+            newGame.printField(AllMoves.NextMove(x,y,mainPlNr));
+//Check for the win
+            if (AllMoves.Winner(mainPlNr)){
+                System.out.println("P"+mainPlNr+" wins!");
                 break;
             }
-            if (plNr==2){
-                plNr=1;
+//Change player
+            if (Game.mainPlNr==2){
+                Game.mainPlNr=1;
             }else{
-                plNr=2;
+                Game.mainPlNr=2;
             }
-
         }while (moveAdd!="Stop");
 
     }
--- src/com/accenture/prebootcamp/tictactoe/game/Moves.java
+++ src/com/accenture/prebootcamp/tictactoe/game/Moves.java
@@ -11,7 +11,7 @@ private String[][] moves=new String[3][3];
         }
         return moves;
     }
-//Records move into array
+//-----------Records move into array-------------
     String[][]NextMove(int x, int y, int plNr){
         String sign;
 //Based on a player selects a sign
@@ -20,23 +20,34 @@ private String[][] moves=new String[3][3];
         }else{
             sign="O";
         }
-//validity check
+//validity check. Check for viable numbers and epmty array slot
         if ((x<=3)&&(y<=3)&&(moves[x-1][y-1]==" ")){
             moves[x-1][y-1]=sign;
+
         }else{
             System.out.println("Wrong coordinates! Try again!");
+            //Change player
+            if (Game.mainPlNr==2){
+                Game.mainPlNr=1;
+            }else{
+                Game.mainPlNr=2;
+            }
         }
+
         return moves;
     }
 
+// -----------Performs winner check------------
     Boolean Winner(int plNr){
         int check=0;
         String sign;
+//Selects sign based on a player
         if(plNr==1){
             sign="X";
         }else{
             sign="O";
         }
+//runs over all rows
         for (int i=0; i<3; i++){
             for (int j=0;j<3;j++){
                 if (moves[i][j]==sign){
@@ -49,7 +60,7 @@ private String[][] moves=new String[3][3];
                 check=0;
             }
         }
-
+//runs over all columns
         for (int i=0; i<3; i++){
             for (int j=0;j<3;j++){
                 if (moves[j][i]==sign){
@@ -62,6 +73,7 @@ private String[][] moves=new String[3][3];
                 check=0;
             }
         }
+//runs over first diagonal
         for (int i=0; i<3; i++){
                 if (moves[i][i]==sign){
                     check++;
@@ -72,7 +84,7 @@ private String[][] moves=new String[3][3];
             }else{
                 check=0;
             }
-
+//runs over second diagonal
         for (int i=0; i<3; i++){
             if (moves[2-i][i]==sign){
                 check++;
@@ -83,6 +95,7 @@ private String[][] moves=new String[3][3];
         }else{
             check=0;
         }
+//no win
         return Boolean.FALSE;
     }
 }
