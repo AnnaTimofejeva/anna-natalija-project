dddd10fed242f0ff9e90bd7504dfa711896adcf3
NatalijaBudnik <71456358+NatalijaBudnik@users.noreply.github.com>
2020-09-29 21:15:27 +0300

    done by me2

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -11,9 +11,8 @@ public class Game {
 
         Board board = new Board();
         boolean gameState = true;
-        int move=0;
+        int move=1;
         do{
-            move++;
             int currentPlayer = number(gameState);
 
             System.out.println("Your move - player " + currentPlayer);
@@ -23,26 +22,21 @@ public class Game {
             //     1 1
             //     instead of:
             //     11
-            int lastCh=userInput.length();
+            int x, y;
 
-            int x;
-            int y;
-            try {
-                lastCh = 3;
-
+            if (isNumeric(userInput)&&userInput.replace(" ","").length()==2){
                 x = Integer.parseInt(userInput.trim().substring(0, 1));
-                y = Integer.parseInt(userInput.trim().substring(lastCh - 1, lastCh));
-
-            } catch (Exception e){
-                System.out.println("Check your input! Use Row Space Column e.g. 1 2");
-                move--;
+                y = Integer.parseInt(userInput.trim().substring(2, 3));
+            } else {
+                System.out.println("Check your input! You should input 3 characters - RowNr Space ColumnNr e.g. 1 2");
                 continue;
             }
+
+
             String playMaker = marker(gameState);
 
             if (!board.makeMove(x, y, playMaker)) {
                 System.out.println("Wrong coordinates! Try again!");
-                move--;
                 continue;
             }
 
@@ -53,10 +47,8 @@ public class Game {
             gameState = !gameState;
             board.print();
 
-            if (move==9){
-                System.out.println("It is a draw!");
-            }
-        } while (move<9);
+        } while (++move<9);
+        System.out.println("It is a draw!");
     }
 
     static String marker(boolean playerSwitch) {
@@ -66,5 +58,15 @@ public class Game {
     static int number(boolean playerSwitch) {
         return playerSwitch ? 1 : 2;
     }
+
+    private static boolean isNumeric(String input) {
+        try {
+            Integer.parseInt(input.replace(" ",""));
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+        return true;
+    }
+
 
 }
